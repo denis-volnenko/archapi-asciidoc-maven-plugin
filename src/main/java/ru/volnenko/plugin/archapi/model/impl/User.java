f9f5e25d46aeee89841236230c46bb3bab2fcc30
Denis Volnenko <denis@volnenko.ru>
2025-04-05 06:00:31 +0400

    Project updated.

---
+++ src/main/java/ru/volnenko/plugin/archapi/model/impl/User.java
@@ -0,0 +1,16 @@
+package ru.volnenko.plugin.archapi.model.impl;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class User {
+
+    private String name;
+
+    private String description;
+
+}
