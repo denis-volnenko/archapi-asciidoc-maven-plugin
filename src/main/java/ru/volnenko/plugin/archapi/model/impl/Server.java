9b4025b66a39772304409b7e91ea7966e52724a5
Denis Volnenko <denis@volnenko.ru>
2025-04-05 07:01:37 +0400

    Project updated.

--- src/main/java/ru/volnenko/plugin/archapi/model/impl/Server.java
+++ src/main/java/ru/volnenko/plugin/archapi/model/impl/Server.java
@@ -4,8 +4,19 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
 @NoArgsConstructor
 public final class Server extends AbstractModel {
+
+    private String name;
+
+    private String host;
+
+    private String description;
+
+    private List<Integer> ports;
+
 }
