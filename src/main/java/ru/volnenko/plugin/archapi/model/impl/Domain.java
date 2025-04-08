21830beb379be36db81b9734d220b5eaef208ff5
Denis Volnenko <denis@volnenko.ru>
2025-04-08 10:49:54 +0400

    Project updated.

---
+++ src/main/java/ru/volnenko/plugin/archapi/model/impl/Domain.java
@@ -0,0 +1,18 @@
+package ru.volnenko.plugin.archapi.model.impl;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public final class Domain {
+
+    private String name = "";
+
+    private String description = "";
+
+}
