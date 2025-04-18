61893798c7ea1f41f7b226463993b3d569f02c7e
Denis Volnenko <denis@volnenko.ru>
2025-04-18 12:09:45 +0400

    Project updated.

--- src/main/java/ru/volnenko/plugin/archapi/builder/dataapi/IColumnBuilder.java
+++ src/main/java/ru/volnenko/plugin/archapi/builder/dataapi/IColumnBuilder.java
@@ -6,12 +6,12 @@ import ru.volnenko.plugin.archapi.model.dataapi.IColumn;
 public interface IColumnBuilder {
 
     @NonNull
+    IDataApiBuilder dataApi();
+
+    @NonNull
     IColumnBuilder column(@NonNull IColumn column);
 
     @NonNull
     IColumnBuilder name(@NonNull String name);
-
-    @NonNull
-    IDataApiBuilder dataApi();
 
 }
