91b7f1062f19629da14f3ed3ae3cf72d0e26aa8f
Denis Volnenko <denis@volnenko.ru>
2025-04-18 11:48:31 +0400

    Project updated.

--- src/main/java/ru/volnenko/plugin/archapi/builder/GeneratorBuilder.java
+++ src/main/java/ru/volnenko/plugin/archapi/builder/GeneratorBuilder.java
@@ -4,8 +4,12 @@ import lombok.NonNull;
 import ru.volnenko.plugin.archapi.builder.archapi.IArchApiBuilder;
 import ru.volnenko.plugin.archapi.builder.dataapi.IDataApiBuilder;
 import ru.volnenko.plugin.archapi.builder.techapi.ITechApiBuilder;
+import ru.volnenko.plugin.archapi.model.archapi.Root;
 
 public final class GeneratorBuilder implements IGeneratorBuilder {
+
+    @NonNull
+    private Root root = new Root();
 
     @NonNull
     public static IGeneratorBuilder create() {
