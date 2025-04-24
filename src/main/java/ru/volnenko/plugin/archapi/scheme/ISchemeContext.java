fa1823d3d9ffbe1a6756e8c9bb76b4e287d2c2f6
Denis Volnenko <denis@volnenko.ru>
2025-04-24 20:39:11 +0400

    Project updated.

--- src/main/java/ru/volnenko/plugin/archapi/scheme/ISchemeContext.java
+++ src/main/java/ru/volnenko/plugin/archapi/scheme/ISchemeContext.java
@@ -1,4 +1,23 @@
 package ru.volnenko.plugin.archapi.scheme;
 
+import lombok.NonNull;
+import ru.volnenko.plugin.archapi.model.*;
+
 public interface ISchemeContext {
+
+    @NonNull
+    ISchemeContext append(@NonNull IService service);
+
+    @NonNull
+    ISchemeContext append(@NonNull IServer server);
+
+    @NonNull
+    ISchemeContext append(@NonNull IUser user);
+
+    @NonNull
+    ISchemeContext append(@NonNull IEnvironment environment);
+
+    @NonNull
+    ISchemeContext append(@NonNull IConnection connection);
+
 }
--- src/main/java/ru/volnenko/plugin/archapi/scheme/impl/SchemeContext.java
+++ src/main/java/ru/volnenko/plugin/archapi/scheme/impl/SchemeContext.java
@@ -1,7 +1,34 @@
 package ru.volnenko.plugin.archapi.scheme.impl;
 
+import lombok.NonNull;
+import ru.volnenko.plugin.archapi.model.*;
 import ru.volnenko.plugin.archapi.scheme.ISchemeContext;
 
 public final class SchemeContext implements ISchemeContext {
 
+    @NonNull
+    @Override
+    public ISchemeContext append(@NonNull IService service) {
+        return null;
+    }
+
+    @Override
+    public @NonNull ISchemeContext append(@NonNull IServer server) {
+        return null;
+    }
+
+    @Override
+    public @NonNull ISchemeContext append(@NonNull IUser user) {
+        return null;
+    }
+
+    @Override
+    public @NonNull ISchemeContext append(@NonNull IEnvironment environment) {
+        return null;
+    }
+
+    @Override
+    public @NonNull ISchemeContext append(@NonNull IConnection connection) {
+        return null;
+    }
 }
