1dd996a1ac5676cce44972872317e1dcd7876adb
Denis Volnenko <denis@volnenko.ru>
2025-04-25 13:50:15 +0400

    Project updated.

--- src/main/java/ru/volnenko/plugin/archapi/techapi/model/impl/Library.java
+++ src/main/java/ru/volnenko/plugin/archapi/techapi/model/impl/Library.java
@@ -3,4 +3,5 @@ package ru.volnenko.plugin.archapi.techapi.model.impl;
 import ru.volnenko.plugin.archapi.model.impl.AbstractBasicEntity;
 
 public final class Library  extends AbstractBasicEntity {
+
 }
