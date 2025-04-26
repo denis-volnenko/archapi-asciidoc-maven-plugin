38c20e3516f0d31146ac45a297dcfe1fe314dec8
Denis Volnenko <denis@volnenko.ru>
2025-04-26 04:27:23 +0400

    Project updated.

--- src/main/java/ru/volnenko/plugin/archapi/techapi/model/impl/Tool.java
+++ src/main/java/ru/volnenko/plugin/archapi/techapi/model/impl/Tool.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import ru.volnenko.plugin.archapi.model.impl.AbstractBasicEntity;
 import ru.volnenko.plugin.archapi.model.impl.AbstractVersionEntity;
 
 @Getter
