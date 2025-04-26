fe885c08db9364a2416d9d0ff3f81e3c816cec63
Denis Volnenko <denis@volnenko.ru>
2025-04-26 04:48:24 +0400

    Project updated.

--- src/main/java/ru/volnenko/plugin/arch/model/impl/Components.java
+++ src/main/java/ru/volnenko/plugin/arch/model/impl/Components.java
@@ -3,10 +3,12 @@ package ru.volnenko.plugin.arch.model.impl;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 import lombok.Setter;
 import ru.volnenko.plugin.arch.techapi.ITechComponents;
 import ru.volnenko.plugin.arch.techapi.model.impl.*;
 
+import java.util.Collections;
 import java.util.Map;
 
 @Getter
@@ -25,28 +27,38 @@ public final class Components implements ITechComponents {
 
     private Map<String, Tool> tools;
 
+    @NonNull
     @Override
     public Map<String, Framework> frameworks() {
+        if (frameworks == null) return Collections.emptyMap();
         return frameworks;
     }
 
+    @NonNull
     @Override
     public Map<String, Language> languages() {
+        if (languages == null) return Collections.emptyMap();
         return languages;
     }
 
+    @NonNull
     @Override
     public Map<String, Library> libraries() {
+        if (libraries == null) return Collections.emptyMap();
         return libraries;
     }
 
+    @NonNull
     @Override
     public Map<String, Platform> platforms() {
+        if (platforms == null) return Collections.emptyMap();
         return platforms;
     }
 
+    @NonNull
     @Override
     public Map<String, Tool> tools() {
+        if (tools == null) return Collections.emptyMap();
         return tools;
     }
 
